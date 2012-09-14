d480492984953d62bbd29f22748d1468c329158a
Dason Kurkiewicz <dasonk@iastate.edu>
2012-09-14 18:21:55 -0500

    Use xdg-mime for autodetection if possible

--- R/playsong.R
+++ R/playsong.R
@@ -1,9 +1,15 @@
 #' Plays a song
 #' 
-#' A system independent way of playing a song made with make_song
+#' A system independent way of playing a song made with make_song.  If
+#' a player for Wave files hasn't been set using setWavPlayer then the function
+#' tries to autodetect an appropriate player.  
 #' 
-#' @param song The output from make_song.  Any valid Wave object should
-#' work though
+#' @section Details:
+#' The autodetection currently relies on xdg-mime.  If this is not on the
+#' system and a valid Wave player isn't set then a warning will occur.
+#' 
+#' @param song The output from make_song.  Any Wave object is valid
+#' input though.
 #' 
 #' @export
 #' @examples
@@ -12,17 +18,31 @@
 #' playsong(song)
 #' }
 playsong <- function(song){
-    # Actually play the song
-    # Sys.info()["sysname"]
-    if(.Platform['OS.type'] == "windows"){
-        play(song)
-    }else{
-        tmpfile <- if(exists("tmpfile")){tmpfile}else{tempfile(fileext = ".wav")}
-        # Check if it still works with this line
-        on.exit(unlink(tmpfile))
-        writeWave(song, file = tmpfile)
-        play(tmpfile, "xdg-open")
-        #com <- paste("xdg-open", tmpfile)
-        #system(com)
+    
+    # Attempt to play song.  
+    out <- play(song)
+    
+    # Success - exit the function
+    if(out == 0){
+        return(invisible())
+    }
+    
+    # If song couldn't play...
+    
+    # Check if the system has xdg-mime
+    j <- Sys.which("xdg-mime")
+    # If xdg-mime is available then autodetect the player for wav files
+    # and set the player to that
+    if(j != ""){
+        prog <- system("xdg-mime query default audio/x-wav", intern = TRUE)
+        prog <- gsub(".desktop", "", prog, fixed = TRUE)
+        prog.path <- Sys.which(prog)
+        setWavPlayer(prog.path)
+    }
+    
+    out <- play(song)
+    if(out != 0){
+        warning("Wave player is not currently set and autodetection failed\n",
+                "Please set the desired player for Wave fles using the setWavPlayer function")
     }
 }
