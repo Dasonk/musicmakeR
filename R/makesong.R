69f460538c6e009f8e180af8ada23d90dfba51d4
Dason Kurkiewicz <dasonk@iastate.edu>
2012-09-14 18:21:37 -0500

    Autodetect baseduration if possible

--- R/makesong.R
+++ R/makesong.R
@@ -4,12 +4,25 @@
 #' 
 #' @param song_notes A character vector containing valid notes and/or chords.
 #' Currently these need to be of the form "3_A1.B2.C3"
-#' @param baseduration The length (in seconds) each individual note should take
+#' @param baseduration The length (in seconds) each individual note should take.
+#' If this is NULL it will check song_notes for a baseduration attribute and if 
+#' present will use that.  If there is no attribute then it defaults to 1/4.
 #' @export
 #' @examples
 #' song <- c("A5", "B5", "C5", "B5", "A5")
 #' mysong <- makesong(song)
-makesong <- function(song_notes, baseduration = 1/4){
+makesong <- function(song_notes, baseduration = NULL){
+    if(is.null(baseduration)){
+        # If there is a baseduration attribute then use that
+        tmp <- attr(song_notes, "baseduration")
+        if(!is.null(tmp)){
+            baseduration <- tmp
+        }else{
+            # Default if nothing is specified and there isn't
+            # a baseduration attribute
+            baseduration <- 1/4
+        }
+    }
     # Turn the definition into Wave objects
     waves <- lapply(song_notes, convert_to_wave, duration = baseduration)
     # Smooth out the transitions
