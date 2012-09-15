05eb863a54cef009e7cff722e398aff27c429d01
Dason Kurkiewicz <dasonk@iastate.edu>
2012-09-14 22:18:15 -0500

    Modified parameter name.

--- R/makesong.R
+++ R/makesong.R
@@ -2,19 +2,19 @@
 #' 
 #' Makes a song from a vector of notes/chords
 #' 
-#' @param song_notes A character vector containing valid notes and/or chords.
+#' @param songnotes A character vector containing valid notes and/or chords.
 #' Currently these need to be of the form "3_A1.B2.C3"
 #' @param baseduration The length (in seconds) each individual note should take.
-#' If this is NULL it will check song_notes for a baseduration attribute and if 
+#' If this is NULL it will check songnotes for a baseduration attribute and if 
 #' present will use that.  If there is no attribute then it defaults to 1/4.
 #' @export
 #' @examples
 #' song <- c("A5", "B5", "C5", "B5", "A5")
 #' mysong <- makesong(song)
-makesong <- function(song_notes, baseduration = NULL){
+makesong <- function(songnotes, baseduration = NULL){
     if(is.null(baseduration)){
         # If there is a baseduration attribute then use that
-        tmp <- attr(song_notes, "baseduration")
+        tmp <- attr(songnotes, "baseduration")
         if(!is.null(tmp)){
             baseduration <- tmp
         }else{
@@ -24,7 +24,7 @@ makesong <- function(song_notes, baseduration = NULL){
         }
     }
     # Turn the definition into Wave objects
-    waves <- lapply(song_notes, convert_to_wave, duration = baseduration)
+    waves <- lapply(songnotes, convert_to_wave, duration = baseduration)
     # Smooth out the transitions
     smoothed_waves <- suppressWarnings(lapply(waves, prepComb))
     # Bind the Waves together
